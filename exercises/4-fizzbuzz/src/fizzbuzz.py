5f8616649caca3a975eb69596e7cd3b2b5375ba3
Max <maxime.virgittiponge@ecole-isitech.fr>
2025-07-29 09:20:07 +0200

    print fizzbuzz

--- exercises/4-fizzbuzz/src/fizzbuzz.py
+++ exercises/4-fizzbuzz/src/fizzbuzz.py
@@ -1,2 +1,12 @@
 def fizzbuzz(number: int) -> str:
+    if number % 3 == 0 and number % 5 == 0 :
+        return "FizzBuzz"
+    elif number % 3 == 0 or "3" in str(number) :
+        return "Fizz"
+    elif number % 5 == 0 or "5" in str(number) :
+        return "Buzz"
     return str(number)
+
+def fizzbuzz2 ():
+    for i in range(1,101) :
+        print(fizzbuzz(i))
--- exercises/4-fizzbuzz/test_fizzbuzz.py
+++ exercises/4-fizzbuzz/test_fizzbuzz.py
@@ -1,4 +1,18 @@
 from src import fizzbuzz
 
-# def test_fizzbuzz():
-#     assert fizzbuzz(1)
+
+
+def test_1_should_return_1():
+    assert fizzbuzz(1) == '1'
+
+def test_2_should_return_2():
+    assert fizzbuzz(2) == '2'
+
+def test_3_should_return_3():
+    assert fizzbuzz(3) == 'Fizz'
+
+def test_4_should_return_4():
+    assert fizzbuzz(5) == 'Buzz'
+
+def test_5_should_return_5() :
+    assert fizzbuzz(15) == 'FizzBuzz'
