713ad035881bb2835a1e5c4d0d3ae0738d58d32d
Max <maxime.virgittiponge@ecole-isitech.fr>
2025-07-30 11:16:40 +0200

    print romanNumeral

--- exercises/5-romanconverter/src/romanNumeral.py
+++ exercises/5-romanconverter/src/romanNumeral.py
@@ -21,6 +21,31 @@ def jn(number: int) -> str:
 
     return "".join(result)
 
+def chiffres(number: str) -> int:
+
+    chiffre_map = [
+        (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+        (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+        (10, "X"), (9, "IX"), (5, "V"), (4, "IV"),
+        (1, "I")
+    ]
+
+    total = 0
+    i = 0
+
+    while i < len(number):
+        for value, roman_symbol in chiffre_map:
+            if number[i:i + len(roman_symbol)] == roman_symbol:
+                total += value
+                i += len(roman_symbol)
+                break
+    return total
+
+
+
+
+
+
 
 
 
--- exercises/5-romanconverter/test_romanNumeral.py
+++ exercises/5-romanconverter/test_romanNumeral.py
@@ -27,4 +27,26 @@ def test_8_should_return_8() :
 def test_9_should_return_9() :
     assert jn(445) == 'CDXLV'
 
+def test_10_should_return_10() :
+    assert chiffres('I') == 1
 
+def test_11_should_return_11() :
+    assert chiffres('II') == 2
+
+def test_12_should_return_12() :
+    assert chiffres('III') == 3
+
+def test_13_should_return_13() :
+    assert chiffres('IV') == 4
+
+def test_14_should_return_14() :
+    assert chiffres('V') == 5
+
+def test_15_should_return_15() :
+    assert chiffres('X') == 10
+
+def test_16_should_return_16() :
+    assert chiffres('CM') == 900
+
+def test_17_should_return_17() :
+    assert chiffres('CMXLIX') == 949
